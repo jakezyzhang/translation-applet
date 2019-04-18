9d448276d1c134a0c22972a4d88af72df523f25a
Rob <378659373@qq.com>
2019-04-18 14:35:12 +0800

    update of list

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ App({
             success: res => {
               // 可以将 res 发送给后台解码出 unionId
               this.globalData.userInfo = res.userInfo
-              console.log(111111111);
+              console.log(res.userInfo);
               // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
               // 所以此处加入 callback 以防止这种情况
               if (this.userInfoReadyCallback) {
@@ -49,7 +49,7 @@ App({
           })
         } else {
           wx.reLaunch({
-            url: '/pages/authorize/authorize',
+            url: '/pages/translation/index',
           })
         }
       }
--- pages/list/list.js
+++ pages/list/list.js
@@ -2,62 +2,7 @@ const app = getApp();
 Page({
   data: {
     //判断小程序的API，回调，参数，组件等是否在当前版本可用。
-    canIUse: wx.canIUse('button.open-type.getUserInfo'),
-    StatusBar: app.globalData.StatusBar,
-    CustomBar: app.globalData.CustomBar,
-    iconList: [{
-      icon: 'cardboardfill',
-      color: 'red',
-      badge: 120,
-      name: 'VR'
-    }, {
-      icon: 'recordfill',
-      color: 'orange',
-      badge: 1,
-      name: '录像'
-    }, {
-      icon: 'picfill',
-      color: 'yellow',
-      badge: 0,
-      name: '图像'
-    }, {
-      icon: 'noticefill',
-      color: 'olive',
-      badge: 22,
-      name: '通知'
-    }, {
-      icon: 'upstagefill',
-      color: 'cyan',
-      badge: 0,
-      name: '排行榜'
-    }, {
-      icon: 'clothesfill',
-      color: 'blue',
-      badge: 0,
-      name: '皮肤'
-    }, {
-      icon: 'discoverfill',
-      color: 'purple',
-      badge: 0,
-      name: '发现'
-    }, {
-      icon: 'questionfill',
-      color: 'mauve',
-      badge: 0,
-      name: '帮助'
-    }, {
-      icon: 'commandfill',
-      color: 'purple',
-      badge: 0,
-      name: '问答'
-    }, {
-      icon: 'brandfill',
-      color: 'mauve',
-      badge: 0,
-      name: '版权'
-    }],
-    gridCol:3,
-    skin: false
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
   showModal(e) {
     this.setData({
@@ -68,16 +13,6 @@ Page({
     this.setData({
       modalName: null
     })
-  },
-  gridchange: function (e) {
-    this.setData({
-      gridCol: e.detail.value
-    });
-  },
-  gridswitch: function (e) {
-    this.setData({
-      gridBorder: e.detail.value
-    });
   },
   menuBorder: function (e) {
     this.setData({
@@ -93,41 +28,6 @@ Page({
     this.setData({
       menuCard: e.detail.value
     });
-  },
-  switchSex: function (e) {
-    this.setData({
-      skin: e.detail.value
-    });
-  },
-
-  // ListTouch触摸开始
-  ListTouchStart(e) {
-    this.setData({
-      ListTouchStart: e.touches[0].pageX
-    })
-  },
-
-  // ListTouch计算方向
-  ListTouchMove(e) {
-    this.setData({
-      ListTouchDirection: e.touches[0].pageX - this.data.ListTouchStart > 0 ? 'right' : 'left'
-    })
-  },
-
-  // ListTouch计算滚动
-  ListTouchEnd(e) {
-    if (this.data.ListTouchDirection =='left'){
-      this.setData({
-        modalName: e.currentTarget.dataset.target
-      })
-    } else {
-      this.setData({
-        modalName: null
-      })
-    }
-    this.setData({
-      ListTouchDirection: null
-    })
   },
   onLoad: function () {
     var that = this;
